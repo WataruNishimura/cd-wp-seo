0064264a924bdc836463b321c32d7aff4ae026d4
WataruNishimura <wataru.nishimura@kraftsman.jp>
2021-11-18 10:28:57 +0900

    cd-wp-seo

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Cd_Wp_Seo
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
